67b308e19a078a3e98cf09b813e10dcf4bb9555d
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-11-21 17:25:04 +0530

    Added Hello world

--- FirstDay/src/Main.java
+++ FirstDay/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World! from FDC");
+
+        System.out.println("Hello, World! from FDC and Jerry");
     }
 }
